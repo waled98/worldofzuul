4ba1731ce7daf19c4cab2f26a7bba691c307c99f
Sören Schwerk <s.schwerk@schule.bremen.de>
2020-12-28 17:14:15 +0100

    init

---
+++ src/de/szut/zuul/ZuulUI.java
@@ -0,0 +1,14 @@
+package de.szut.zuul;
+
+public class ZuulUI {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.play();
+    }
+}
+
+
