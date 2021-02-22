a48b928ed58e8806f0d591730aed9a6226abfad3
Sören Schwerk <s.schwerk@schule.bremen.de>
2021-02-22 13:38:26 +0100

    help geaendert

--- src/de/szut/zuul/Game.java
+++ src/de/szut/zuul/Game.java
@@ -146,7 +146,7 @@ public class Game
     private void printHelp() 
     {
         System.out.println("You are lost. You are alone. You wander");
-        System.out.println("around at the university.");
+        System.out.println("through the jungle. At once there is a glade. On it there a buildings...");
         System.out.println();
         System.out.println("Your command words are:");
         System.out.println("   go quit help");
